26bafbc5d383f4866019de1c47647336f14ca384
Lewis lee <minecraftpe132006@gmail.com>
2025-03-06 13:52:08 +0800

    fix: added search & replace lines for php-fpm configuration to limit permissions and set the port number that fpm is listening to

--- requirements/wordpress/scripts/entrypoint.sh
+++ requirements/wordpress/scripts/entrypoint.sh
@@ -6,7 +6,11 @@ wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USE
 
 wp user create $WP_USER_LOGIN $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author --allow-root --path=/var/www/html/
 
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/1' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/;listen.owner = www-data/listen.owner = www-data/1' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/;listen.group = www-data/listen.group = www-data/1' /etc/php/7.4/fpm/pool.d/www.conf
 
+mkdir /run/php
 
 #/usr/sbin/php-fpm7.3 -F
 tail -f /dev/null
