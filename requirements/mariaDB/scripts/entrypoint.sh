2f2c6ed13e51df11bb91c9dcdad1056ef5ab36d7
Lewis lee <minecraftpe132006@gmail.com>
2025-03-06 09:38:37 +0800

    fix: renamed script.sh to entrypoint.sh

--- requirements/mariaDB/scripts/entrypoint.sh
+++ requirements/mariaDB/scripts/entrypoint.sh
@@ -8,7 +8,7 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > dbInit.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> dbInit.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> dbInit.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASS' ;" >> dbInit.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456' ;" >> dbInit.sql
 echo "FLUSH PRIVILEGES; " >> dbInit.sql
 
 mysql < dbInit.sql
---
+++ requirements/wordpress/scripts/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir /var/www/
+mkdir /var/www/html
+
+cd /var/www/html
+
+rm -rf *
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+chmod +x wp-cli.phar 
+
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
+/usr/sbin/php-fpm7.3 -F
