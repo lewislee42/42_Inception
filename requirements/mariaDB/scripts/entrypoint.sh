7d5069b8369e1f08619d02db2ed235db918fb94a
lewislee42 <minecraftpre132006@gmail.com>
2025-03-07 11:37:53 +0800

    fixed: mariadb was not initializing user properly now fixed

--- requirements/mariaDB/scripts/entrypoint.sh
+++ requirements/mariaDB/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 sed -i "s/\= mysql/\= root/1" /etc/mysql/mariadb.conf.d/50-server.cnf # could be a security risk
 sed -i "s/\= 127\.0\.0\.1/\= 0\.0\.0\.0/1" /etc/mysql/mariadb.conf.d/50-server.cnf # could be a security risk
 
-service mysql start
+service mariadb start
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > dbInit.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> dbInit.sql
@@ -11,6 +11,6 @@ echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> dbInit.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456' ;" >> dbInit.sql
 echo "FLUSH PRIVILEGES; " >> dbInit.sql
 
-mysql < dbInit.sql
+mariadb < dbInit.sql
 kill $(cat /var/run/mysqld/mysqld.pid)
 mysqld
