4dc93783790b4a454afed2d2437329aed66981bb
lewislee42 <minecraftpre132006@gmail.com>
2025-03-14 11:29:55 +0800

    feat: made it so that the wordpress installation did not require root account anymore as it is discourage to do so & renamed entrypoint script & deleted dockerignore file

---
+++ srcs/requirements/wordpress/tools/wp-install-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wp core download
+
+# create the config file with our database
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb
+
+# install the wordpress website files
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+
+# create a user using the wordpress cli
+wp user create $WP_USER_LOGIN $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author
