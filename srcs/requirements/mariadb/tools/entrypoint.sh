a9af3b5ffbc334b35aab1a3a4e2a6a6b2371e71a
Lewis lee <minecraftpe132006@gmail.com>
2025-03-18 13:14:51 +0800

    fix: added new env for mariadb root password

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -10,7 +10,7 @@ service mariadb start
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > dbInit.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> dbInit.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> dbInit.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456' ;" >> dbInit.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS' ;" >> dbInit.sql
 echo "FLUSH PRIVILEGES; " >> dbInit.sql
 
 # runs the script
